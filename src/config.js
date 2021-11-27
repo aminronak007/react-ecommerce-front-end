4c2a807cfe4ec2b5b02d37f6b9aaac43102aacee
Ronak Amin <60923453+aminronak007@users.noreply.github.com>
2021-11-27 11:03:57 +0530

    Delete config.js

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const url = "http://localhost:3000";
-export const frontUrl = "http://localhost:5000";
-export const STRIPE_KEY =
-  "pk_test_51JzJfSSDiSNQp2yK1mbzLHEjkH078YUVVPtDTfwUUqz1PW3snvhaTgcMsr4DmnknYqZz5QKguicnpAgfmhOWjQhh00GV5sHugO";
